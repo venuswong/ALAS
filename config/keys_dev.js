831119927278bb97f3e67cc3e254077c0c28ab7f
Alec Maier <Maier.144@osu.edu>
2019-08-20 19:06:26 -0400

    updated keys_dev

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,6 +1,6 @@
 module.exports = {
-    host: 'alasdbinstance123.cgdbunnaevjk.us-east-2.rds.amazonaws.com',
-    user: 'alasadmin',
-    password: 'forthekids123',
-    database: 'ALAS'
+    host: 'localhost',
+    user: 'CHANGEME',
+    password: 'CHANGEME',
+    database: 'CHANGEME'
 };
